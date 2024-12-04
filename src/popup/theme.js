58fb625c8e80d9d0fdc88c05be5c87da3e2a9a87
Dora Zhao <dorazhao@DNa80d5dc.SUNet>
2024-12-03 22:21:41 -0800

    Update popup theme

--- src/popup/theme.js
+++ src/popup/theme.js
@@ -1,29 +1,39 @@
 import { createTheme } from '@mui/material/styles';
+import './theme.css'; 
 
 const theme = createTheme({
-  palette: {
+    typography: {
+        fontFamily: [
+          'Lato',
+          'Roboto',
+          '"Helvetica Neue"',
+          'Arial',
+          'sans-serif'
+        ].join(','),
+    },
+    palette: {
     primary: {
-      main: '#7091E6', // Primary color
-      contrastText: '#fff', // Text color for primary
+        main: '#7091E6', // Primary color
+        contrastText: '#fff', // Text color for primary
     },
     secondary: {
-      main: '#ADBBDA', // Secondary color
-      contrastText: '#fff', // Text color for secondary
+        main: '#ADBBDA', // Secondary color
+        contrastText: '#fff', // Text color for secondary
     },
     dark: {
-      default: '#323639', 
+        default: '#323639', 
     },
-  },
-  components: {
+    },
+    components: {
     MuiButton: {
-      styleOverrides: {
+        styleOverrides: {
         root: {
-          borderRadius: '16px', // Rounded corners
-          textTransform: 'none', // Disable uppercase text
+            borderRadius: '16px', // Rounded corners
+            textTransform: 'none', // Disable uppercase text
         },
-      },
+        },
     },
-  },
+    },
 });
 
 export default theme;
