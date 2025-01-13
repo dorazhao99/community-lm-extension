918e42026023c47395a90d98f700152176fd783a
Dora Zhao <dorazhao@Doras-MacBook-Pro.local>
2025-01-13 09:32:24 -0800

    Update regex processing

--- src/contentScripts/router.tsx
+++ src/contentScripts/router.tsx
@@ -1,6 +1,10 @@
 import BM25 from "okapibm25";
 
 
+function escapeRegExp(string:string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+
 const stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now']
 
 function remove_stopwords(str:string) {
@@ -12,7 +16,7 @@ function remove_stopwords(str:string) {
            res.push(word_clean)
        }
     }
-    return res
+    return res.join(' ')
 }  
 
 async function routeDocuments(modules: any, prompt: any) {
@@ -21,14 +25,14 @@ async function routeDocuments(modules: any, prompt: any) {
     const docs = []
     const moduleNames:Array<string> = []
     for (const [key, value] of Object.entries(modules)) {
-        const cleaned = remove_stopwords(value.knowledge).join(' ')
+        const cleaned = escapeRegExp(remove_stopwords(value.knowledge))
         console.log(cleaned)
         docs.push(cleaned)
         moduleNames.push(key)
       }
       
 
-    const q = remove_stopwords(prompt.join(' '))
+    const q = escapeRegExp(remove_stopwords(prompt.join(' '))).split(' ')
     const result = BM25(docs, q, { k1: 1.3, b: 0.9 }) as number[];
 
     const sortedIndices = result
